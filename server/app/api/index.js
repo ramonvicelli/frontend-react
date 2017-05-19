014a433366158feed31ae9030431890d6b9ed9d2
Ramon <ramonvicelli@gmail.com>
2017-05-19 13:08:35 +0100

    [server] create route to book

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -8,29 +8,82 @@ const authors = [{
   },
   {
     id: 2,
-    email: 'vicelli@vicelli.com',
-    name: 'Vicelli',
-    password: '123'
-  },
-  {
-    id: 3,
-    email: 'machado@machado.com',
-    name: 'Machado',
-    password: '123'
-  },
-  {
-    id: 4,
     email: 'breno@breno.com',
     name: 'Breno',
     password: '123'
   },
   {
-    id: 5,
+    id: 3,
     email: 'mauro@mauro.com',
     name: 'Mauro',
     password: '123'
   }
 ];
+
+
+const books = [{
+  id: 1,
+  title: 'The secret of the old clock',
+  price: 10,
+  author: authors[0]
+}, {
+  id: 2,
+  title: 'The fault in our stars',
+  price: 13,
+  author: authors[1]
+}, {
+  id: 3,
+  title: 'My dad is cooler than your dad',
+  price: 10,
+  author: authors[1]
+}, {
+  id: 4,
+  title: 'Lie of Pi',
+  price: 10,
+  author: authors[2]
+}, {
+  id: 5,
+  title: 'Of Mice and men',
+  price: 10,
+  author: authors[0]
+}];
+
+
+API.books = (req, res) => res.json(books);
+API.booksadd = (req, res) => {
+  let book = req.body;
+  if (book.title === '' || book.price === '' || book.authorId === '') {
+    const errors = [];
+    if (! book.title) {
+      errors.push({
+        defaultMessage: "Title can't be blank",
+        field: 'title'
+      });
+    }
+    if (! book.price) {
+      errors.push({
+        defaultMessage: "Price can't be blank",
+        field: 'price'
+      });
+    }
+    if (! book.Id) {
+      errors.push({
+        defaultMessage: "Author can't be blank",
+        field: 'authorId'
+      });
+    }
+    res.status(400);
+    res.json(errors);
+  } else {
+    const lastBook = books.slice(-1)[0];
+    const author = authors.find(author => author.id == book.authorId);
+
+    book.id = lastBook.id + 1;
+    book.author = author;
+    books.push(book);
+    res.json(books);
+  }
+}
 
 API.authors = (req, res) => res.json(authors);
 API.add = (req, res) => {
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
   app.route('/authors')
     .get(api.authors)
     .post(api.add);
+
+  app.route('/books')
+    .get(api.books)
+    .post(api.booksadd);
 };
